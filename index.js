b5daf5c63a38a729d119f177494186a02ea22c73
Deanna Heffelfinger <dheffelf@umail.iu.edu>
2017-06-06 15:48:21 -0400

    day two

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const personForm = document.querySelector('#personForm')
 
+function renderColor(color){
+    const div = document.createElement('div')
+    div.style.backgroundColor = color 
+    div.style.width = '100px'
+    div.style.height = '50px'
+    return div 
+    
+    //`
+   // <div style="background-color: ${favoriteColor}; width: 100px; height: 50px;"></div>
+   //  `
+
+}
 
 function handleSubmit(ev){
     ev.preventDefault()
@@ -7,18 +19,23 @@ function handleSubmit(ev){
     const details = document.querySelector('#details')
     const name = f.personName.value
     const favoriteColor = f.favoriteColor.value
-    const age = f.age.value
-
-    const colorDiv = `
-    <div style="background-color: ${favoriteColor}; width 100px; height: 50px;"></div>
-    `
+    const age = f.age.value 
 
 
 
-   // const boldedName = document.createElement('strong')
-   // boldedName.textContent = name 
+    const list = document.createElement('ul')
 
-   // details.appendChild(boldedName)
+
+
+    const colorItem = document.createElement('li')
+    //outerHTML is within brackets 
+    colorItem.innerHTML = `Favorite Color: ${renderColor(favoriteColor).outerHTML}`
+
+    //get empty element 
+    //boldedName.textContent = name 
+    //puts name in it 
+    //details.appendChild(boldedName)
+    //puts into details, actualy object 
 
    details.innerHTML = `
     <ul>
